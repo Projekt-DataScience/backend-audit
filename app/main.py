035c287fc845d0536b0b8b2c28612cd85f79c3b4
Fahri Korkmaz <fahri.korkmaz@protonmail.ch>
2022-12-09 08:59:34 +0100

    Fix backend db lib import

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 
+from backend_db_lib.models import base, LPAAudit
+from backend_db_lib.manager import DatabaseManager
+
+DATABASE_URL = "postgresql://backendgang:backendgang@db:8010/backend"
+
+dbm = DatabaseManager(base, DATABASE_URL)
 app = FastAPI()
 
 @app.get("/healthcheck")
 def healthcheck():
     return { "Status": "Up" }
+
+@app.get("/audit")
+def get_audits(authorization: str | None = Header(default=None)):
+    pass
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
