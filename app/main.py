12802c1a504880ebc44cd668f16b515b34f35bc3
Fahri Korkmaz <fahri.korkmaz@protonmail.ch>
2022-12-09 19:06:41 +0100

    Add CRUD for LPA Answer Reason

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ from fastapi import FastAPI
 from routes.lpa_question_category import router as lpa_question_category_router
 from routes.lpa_question import router as lpa_question_router
 from routes.lpa_audit import router as lpa_audit_router
+from routes.audit_answers import router as lpa_answer_router
 
 app = FastAPI()
 app.include_router(lpa_question_category_router)
 app.include_router(lpa_question_router)
 app.include_router(lpa_audit_router)
+app.include_router(lpa_answer_router)
 
 @app.get("/")
 @app.get("/healthcheck")
---
+++ app/routes/audit_answers.py
@@ -0,0 +1,60 @@
+from fastapi import APIRouter, HTTPException
+
+from backend_db_lib.models import LPAAnswerReason
+from dao.lpa_answer import LPAAnswerReasonDAO
+from db import dbm
+
+router = APIRouter(
+    prefix="/lpa_answer",
+    tags=["lpa_answer"],
+    responses={404: {"description": "Not found"}},
+)
+
+@router.get("/reason")
+def get_lpa_answer_reasons():
+    with dbm.create_session() as session:
+        answer_reasons = session.query(LPAAnswerReason).all()
+
+    return answer_reasons
+
+@router.post("/reason")
+def create_lpa_answer_reason(answer: LPAAnswerReasonDAO):
+    with dbm.create_session() as session:
+        answer = LPAAnswerReason(
+            description = answer.description
+        )
+
+        session.add(answer)
+        session.flush()
+        session.commit()
+        session.refresh(answer)
+
+    return answer
+
+@router.post("/reason/{id}")
+def update_lpa_answer_reason(answer: LPAAnswerReasonDAO, id: int):
+    with dbm.create_session() as session:
+        answer_reason = session.query(LPAAnswerReason).get(id)
+        if answer_reason is None:
+            raise HTTPException(status_code=404, detail="LPA Answer Reason not found")
+
+        answer_reason.description = answer.description
+
+        session.add(answer_reason)
+        session.flush()
+        session.commit()
+        session.refresh(answer_reason)
+
+    return answer_reason
+
+@router.post("/reason/delete/{id}")
+def delete_lpa_answer_reason(id: int):
+    with dbm.create_session() as session:
+        answer_reason = session.query(LPAAnswerReason).get(id)
+        if answer_reason is None:
+            raise HTTPException(status_code=404, detail="LPA Answer Reason not found")
+
+        session.delete(answer_reason)
+        session.commit()
+
+    return id
