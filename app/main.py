56e72e39a1681f0d0aa6bbf9ec1664aed6b26d0e
Korkmatik <git.c8tmx@aleeas.com>
2022-12-12 21:39:26 +0100

    Add CORS policy

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import uvicorn
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.lpa_question_category import router as lpa_question_category_router
 from routes.lpa_question import router as lpa_question_router
@@ -7,8 +9,15 @@ from routes.lpa_audit import router as lpa_audit_router
 from routes.audit_answers import router as lpa_answer_router
 from routes.recurrence import router as recurrence_router
 
-
 app = FastAPI(docs_url="/api/audit/docs", redoc_url="/api/audit/redoc", openapi_url="/api/audit/openapi.json")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(lpa_question_category_router)
 app.include_router(lpa_question_router)
@@ -16,10 +25,11 @@ app.include_router(lpa_audit_router)
 app.include_router(lpa_answer_router)
 app.include_router(recurrence_router)
 
+
 @app.get("/api/audit/")
 @app.get("/api/audit/healthcheck")
 def healthcheck():
-    return { "Status": "Up" }
+    return {"Status": "Up"}
 
 
 if __name__ == "__main__":
